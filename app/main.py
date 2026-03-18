12828fee9465fd75a6a45b57f49193ad8df1f65b
ch3p4ll3 <slinucs@gmail.com>
2026-03-18 11:35:09 +0100

    docker file

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
+from os import getenv
+
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 from app.routes import piastrella
 
+
 app = FastAPI()
+env = getenv("ENV", "development")
+
+if env == "docker":
+    title = "Piastrella API (Docker) - Jonatan loves Docker"
+    print(title)
+    app.title = title
 
 app.include_router(piastrella.router, prefix="/piastrella")
